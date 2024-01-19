453a317eb262942f1a65e9335e9e0aa340c88d99
RodriguesCarlos01 <cacaalcides7@gmail.com>
2024-01-19 12:44:10 -0300

    Implementando lógica

---
+++ goGuard.js
@@ -0,0 +1,10 @@
+function calculate(){
+    let va = document.querySelector('#vaValue')
+    let shifts = document.querySelector('#shifts')
+    let result = document.querySelector('#result')
+ 
+    result.innerHTML = va.value * shifts.value
+    
+ }
+
+ 
